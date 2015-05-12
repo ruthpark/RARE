b8a88bb94dd81dd48d2e21aa9ab8845f224e4472
Alex H <alexh95@mit.edu>
2015-05-12 14:30:49 -0400

    Added comment for script

--- software.sh
+++ software.sh
@@ -1,3 +1,5 @@
+#This script installs the necessary software on the laptop 
+
 #video editor
 sudo add-apt-repository ppa:openshot.developers/ppa
 sudo apt-get update && sudo apt-get install openshot openshot-doc
